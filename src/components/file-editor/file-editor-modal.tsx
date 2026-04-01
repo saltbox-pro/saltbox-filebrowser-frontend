2369c274eeb24b865ba8183769d8ca136362ce45
Igor Eliseev <i.eliseev@saltbox.pro>
2026-04-01 12:14:15 +0300

    feat: configure monaco editor loader

--- src/components/file-editor/file-editor-modal.tsx
+++ src/components/file-editor/file-editor-modal.tsx
@@ -1,6 +1,9 @@
 import { Button, Modal, Spin, Tag, message } from "antd";
-import Editor, { type OnMount } from "@monaco-editor/react";
+import Editor, { loader, type OnMount } from "@monaco-editor/react";
+import * as monaco from "monaco-editor";
 import type { editor } from "monaco-editor";
+
+loader.config({ monaco });
 import { observer } from "mobx-react";
 import { useCallback, useRef } from "react";
 import { useTranslation } from "react-i18next";
