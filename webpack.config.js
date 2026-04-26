5de3aab567d7db5e69a66f5cdc78b619ca57bf36
Igor Eliseev <i.eliseev@saltbox.pro>
2026-04-26 20:59:06 +0300

    feat: rework webpack config for add hash-named chunks and change cache politics in nginx conf

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,33 @@ const singleSpaDefaults = require("webpack-config-single-spa-react-ts");
 const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
 const path = require("path");
 
+class EmitEntryShimPlugin {
+  constructor(opts) {
+    this.shimName = opts.shimName;
+    this.entryName = opts.entryName || "main";
+  }
+  apply(compiler) {
+    const pluginName = "EmitEntryShimPlugin";
+    compiler.hooks.thisCompilation.tap(pluginName, (compilation) => {
+      compilation.hooks.processAssets.tap(
+        { name: pluginName, stage: compiler.webpack.Compilation.PROCESS_ASSETS_STAGE_SUMMARIZE },
+        () => {
+          const entrypoint = compilation.entrypoints.get(this.entryName);
+          if (!entrypoint) return;
+          const entryChunk = entrypoint.getEntrypointChunk();
+          const jsFile = [...entryChunk.files].find((f) => f.endsWith(".js"));
+          if (!jsFile || jsFile === this.shimName) return;
+          const shim = `export * from "./${jsFile}";\n`;
+          compilation.emitAsset(this.shimName, new compiler.webpack.sources.RawSource(shim));
+        }
+      );
+    });
+  }
+}
+
 module.exports = (webpackConfigEnv, argv) => {
   const isDev = argv.mode === "development";
+  const isProd = argv.mode === "production";
 
   const defaultConfig = singleSpaDefaults({
     orgName: "saltbox",
@@ -25,12 +50,36 @@ module.exports = (webpackConfigEnv, argv) => {
     },
     plugins: [
       new MonacoWebpackPlugin({
-        filename: "[name].worker.js",
+        filename: "[name].[contenthash].worker.js",
         publicPath: isDev ? "http://localhost:4206/" : undefined,
       }),
-    ],
+      isProd && new EmitEntryShimPlugin({ shimName: "index.js" }),
+    ].filter(Boolean),
     output: {
-      filename: 'index.js',
+      filename: isProd ? "index.[contenthash].js" : "index.js",
+      chunkFilename: "[name].[contenthash].js",
+      assetModuleFilename: "assets/[name].[contenthash][ext]",
+    },
+    optimization: {
+      moduleIds: "deterministic",
+      chunkIds: "deterministic",
+      runtimeChunk: false,
+      splitChunks: {
+        chunks: "async",
+        cacheGroups: {
+          defaultVendors: {
+            test: /[\\/]node_modules[\\/]/,
+            name: "vendors",
+            priority: -10,
+            reuseExistingChunk: true,
+          },
+          default: {
+            minChunks: 2,
+            priority: -20,
+            reuseExistingChunk: true,
+          },
+        },
+      },
     },
   });
 
